db8da5b9a9b3da13b5ced03087163c4c3d1dbc2a
Michaelbretagne <michael.bretagne@gmail.com>
2016-11-08 13:39:19 -0800

    Add New York Time ajax request

--- js/script.js
+++ js/script.js
@@ -21,7 +21,33 @@ function loadData() {
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    // New York Time ajax request
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
+    url += '?' + $.param({
+        'q': cityStr,
+        'sort': "newest",
+        'api-key': "MyAPIkey"
+    });
+
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(data) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        console.log(articles.length);
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' +
+                '<a href="' + article.web_url + '">' +
+                article.headline.main + '</a>' +
+                '<p>' + article.snippet + '</p>' + '</li>');
+        };
+    }).fail(function(err) {
+        throw err;
+    });
 
     return false;
 };
